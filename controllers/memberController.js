9c72edf90a9f7e37cb674e45d634c7348be141d5
ortigaliev <ortigaliyevm@gmail.com>
2023-10-19 16:17:13 +0900

    feat: member login process development

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -5,21 +5,30 @@ const Member = require("../models/Member");
 memberController.signup = async (req, res) => {
   try{
     console.log("POST: cont/signup");
-    const data = req.body;
-    const member = new Member();
-    const new_member = await member.signupData(data);
+    const data = req.body,
+    member = new Member(),
+    new_member = await member.signupData(data);
 
-
-    res.send("done");
+    res.json({state: "succeed", data: new_member});
   } catch (err) {
     console.log(`ERROR, const/signup, ${err.message}`);
+    res.json({state: "fail", message: err_message});
   }
 };
 
 //login controller
-memberController.login = (req, res) => {
-  console.log("POST contr.login");//routerdan kirib kelayotgan request
-  res.send("login sahifasidasiz");
+memberController.login = async (req, res) => {
+  try{
+    console.log("POST: cont/login");
+    const data = req.body,
+    member = new Member(),
+    result = await member.loginData(data);
+
+    res.json({state: "succeed", data: result});
+  } catch (err) {
+    console.log(`ERROR, const/login, ${err.message}`);
+    res.json({state: "fail", message: err_message});
+  }
 };
 
 //loguot controller
