72e197cf083f831bd5229618462a6faf459ad7a2
ortigaliev <ortigaliyevm@gmail.com>
2023-12-17 23:43:06 +0900

    feat: develop getMemberFollowers following back REST API

--- controllers/followController.js
+++ controllers/followController.js
@@ -5,8 +5,8 @@ let followController = module.exports;
 
 followController.subscribe = async (req, res) => {
   try {
-    console.log("POST: cont/subscribe");
-    assert.ok(req.member, Definer.auth_err5);
+    console.log("POST: cont/subscribe"); //modul(CORE),external,file
+    assert.ok(req.member, Definer.auth_err5);//4 xil 1-bcdagi validation
 
     const follow = new Follow();
     await follow.subscribeData(req.member, req.body);
@@ -44,5 +44,20 @@ followController.getMemberFollowings = async (req, res) => {
   }catch (err) {
     console.log(`ERROR, cont/getMemberFollowings, ${err.message}`);
     res.json({ status: "fail", message: err.message});
+  }
+};
+
+followController.getMemberFollowers = async (req, res) => {
+  try {
+      console.log("GET: cont/getMemberFollowers");
+      const follow = new Follow();
+      const result = await follow.getMemberFollowersData(req.member, req.query);
+
+      res.json({ state: "success", data: result });
+
+  } catch(err) {
+      console.log(`ERROR: cont/getMemberFollowers, ${err.message}`);
+      res.json({state: 'fail', message: err.message});
+
   }
 };
--- models/Follow.js
+++ models/Follow.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const { shapeIntoMongooseObjectId } = require("../lib/config");
+const { shapeIntoMongooseObjectId, lookup_auth_member_following } = require("../lib/config");
 const Definer = require("../lib/mistake");
 const FollowModel = require("../schema/follow.model");
 const MemberModel = require("../schema/member.model");
@@ -18,108 +18,148 @@ class Follow {
       const follow_id = shapeIntoMongooseObjectId(data.mb_id);
 
       const member_data = await this.memberModel
-        .findById({ _id: follow_id })
-        .exec();
+      .findById({ _id: follow_id })
+      .exec();
       assert.ok(member_data, Definer.general_err2);
 
       const result = await this.createSubscriptionData(
         follow_id,
         subscriber_id
-      );
-      assert.ok(result, Definer.general_err1);
+        );
+        assert.ok(result, Definer.general_err1);
 
-      await this.modifyMemberFollowCounts(follow_id, "subscriber_change", 1);
-      await this.modifyMemberFollowCounts(subscriber_id, "follow_change", 1);
-      return true;
-    } catch (err) {
-      throw err;
+        await this.modifyMemberFollowCounts(follow_id, "subscriber_change", 1);
+        await this.modifyMemberFollowCounts(subscriber_id, "follow_change", 1);
+        return true;
+      } catch (err) {
+        throw err;
+      }
     }
-  }
 
-  async createSubscriptionData(follow_id, subscriber_id) {
-    try {
-      const new_follow = new this.followModel({
-        follow_id: follow_id,
-        subscriber_id: subscriber_id,
-      });
-      return await new_follow.save();
-    } catch (mongo_err) {
-      console.log(mongo_err);
-      throw new Error(Definer.follow_err2);
+    async createSubscriptionData(follow_id, subscriber_id) {
+      try {
+        const new_follow = new this.followModel({
+          follow_id: follow_id,
+          subscriber_id: subscriber_id,
+        });
+        return await new_follow.save();
+      } catch (mongo_err) {
+        console.log(mongo_err);
+        throw new Error(Definer.follow_err2);
+      }
     }
-  }
 
-  async modifyMemberFollowCounts(mb_id, type, modifier) {
-    try {
-      if (type === "follow_change") {
-        await this.memberModel
+    async modifyMemberFollowCounts(mb_id, type, modifier) {
+      try {
+        if (type === "follow_change") {
+          await this.memberModel
           .findByIdAndUpdate(
             { _id: mb_id },
             { $inc: { mb_follow_cnt: modifier } }
-          )
-          .exec();
-      } else if (type === "subscriber_change") {
-        await this.memberModel
-          .findByIdAndUpdate(
-            { _id: mb_id },
-            { $inc: { mb_subscriber_cnt: modifier } }
-          )
-          .exec();
-      }
-    } catch (err) {
-      throw err;
-    }
-  }
+            )
+            .exec();
+          } else if (type === "subscriber_change") {
+            await this.memberModel
+            .findByIdAndUpdate(
+              { _id: mb_id },
+              { $inc: { mb_subscriber_cnt: modifier } }
+              )
+              .exec();
+            }
+          } catch (err) {
+            throw err;
+          }
+        }
 
-  async unsubscribeData(member, data) {
-    try {
-      const subscriber_id = shapeIntoMongooseObjectId(member._id);
-      const follow_id = shapeIntoMongooseObjectId(data.mb_id);
+        async unsubscribeData(member, data) {
+          try {
+            const subscriber_id = shapeIntoMongooseObjectId(member._id);
+            const follow_id = shapeIntoMongooseObjectId(data.mb_id);
 
-      const result = await this.followModel.findOneAndDelete({
-        follow_id: follow_id,
-        subscriber_id: subscriber_id,
-      });
-      assert.ok(result, Definer.general_err1);
+            const result = await this.followModel.findOneAndDelete({
+              follow_id: follow_id,
+              subscriber_id: subscriber_id,
+            });
+            assert.ok(result, Definer.general_err1);
 
-      await this.modifyMemberFollowCounts(follow_id, "subscriber_change", -1);
-      await this.modifyMemberFollowCounts(subscriber_id, "follow_change", -1);
+            await this.modifyMemberFollowCounts(follow_id, "subscriber_change", -1);
+            await this.modifyMemberFollowCounts(subscriber_id, "follow_change", -1);
 
-      return true;
-    }catch (err) {
-      throw err;
-    }
-  }
+            return true;
+          }catch (err) {
+            throw err;
+          }
+        }
 
-  async getMemberFollowingsData(inquiry) {
-    try{
-      const subscriber_id = shapeIntoMongooseObjectId(inquiry.mb_id),
-        page = inquiry.page * 1, //paramInt()
-        limit = inquiry.limit * 1;
+        async getMemberFollowingsData(inquiry) {
+          try{
+            const subscriber_id = shapeIntoMongooseObjectId(inquiry.mb_id),
+            page = inquiry.page * 1, //paramInt()
+            limit = inquiry.limit * 1;
 
-    const result = await this.followModel.aggregate([
-      { $match: { subscriber_id: subscriber_id}},
-      { $sort: { createdAt: -1}},
-      { $skip: (page-1) * limit },
-      { $limit: limit },
-      {
-        $lookup: {
-          from: "members",
-          localField: "follow_id",
-          foreignField: "_id",
-          as: "follow_member_data",
-        },
-      },
-      { $unwind: "$follow_member_data"},
-    ])
-    .exec();
+            const result = await this.followModel.aggregate([
+              { $match: { subscriber_id: subscriber_id}},
+              { $sort: { createdAt: -1}},
+              { $skip: (page-1) * limit },
+              { $limit: limit },
+              {
+                $lookup: {
+                  from: "members",
+                  localField: "follow_id",
+                  foreignField: "_id",
+                  as: "follow_member_data",
+                },
+              },
+              { $unwind: "$follow_member_data"},
+            ])
+            .exec();
 
-    assert.ok(result, Definer.follow_err3);
-    return result;
-    }catch (err) {
-      throw err;
-    }
-  }
+            assert.ok(result, Definer.follow_err3);
+            return result;
+          }catch (err) {
+            throw err;
+          }
+        }
+
+        async getMemberFollowersData (member, inquiry) {
+          try {
+            const follow_id = shapeIntoMongooseObjectId(inquiry.mb_id),
+            page = inquiry.page * 1,
+            limit = inquiry.limit * 1;
+
+
+            let aggregateQuery = [
+              { $match: { follow_id: follow_id } },
+              { $sort: { createdAt: -1 } },
+              { $skip: (page - 1) * limit },
+              { $limit: limit },
+              {
+                $lookup: {
+                  from: "members",
+                  localField: 'subscriber_id',
+                  foreignField: '_id',
+                  as: 'subscriber_member_data'
+                },
+              },
+              { $unwind: "$subscriber_member_data"},
+
+            ];
+
+            //following follow back to subscriber
+            if(member && member._id === inquiry.mb_id) {
+              aggregateQuery.push(lookup_auth_member_following(follow_id));
+            }
+
+
+            const result =  await this.followModel.aggregate(aggregateQuery).exec();
+
+            assert.ok(result, Definer.follow_err3);
+            return result;
+          }catch(err) {
+            throw err;
+          }
+        }
+
 
 }
 
